cee0615996a91ed95e26c891e24958c68747fc1d
ProgLuis <aulaendesarrollo@gmail.com>
2024-11-12 17:00:02 -0500

    hoy

---
+++ src/main/java/utp/puestoventa/PuestoVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package utp.puestoventa;
+
+/**
+ *
+ * @author Admin
+ */
+public class PuestoVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
