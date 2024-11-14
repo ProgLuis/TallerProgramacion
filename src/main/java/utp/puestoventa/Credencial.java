c408e29cc558aa98187b508962124f026f049f56
Luis_Baldarrago <luisbaldarragotorreblenca@gmail.com>
2024-11-13 23:01:03 -0500

    13-11-2024 / LuisBaldarrago

---
+++ src/main/java/utp/puestoventa/Credencial.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utp.puestoventa;
+
+/**
+ *
+ * @author Admin
+ */
+public class Credencial {
+        private String usuario;
+    private String contraseña;
+
+    public Credencial() {}
+
+    public Credencial(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
