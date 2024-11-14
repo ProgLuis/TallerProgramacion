32107b822b4eaee51f4fa402ba2086a9a6af550d
ProgLuis <aulaendesarrollo@gmail.com>
2024-11-13 23:25:57 -0500

    13-11-2024 / Enrique

--- src/main/java/utp/puestoventa/PuestoVenta.java
+++ src/main/java/utp/puestoventa/PuestoVenta.java
@@ -11,6 +11,21 @@ package utp.puestoventa;
 public class PuestoVenta {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+         // Creamos nueva instancia del formulario llamado: Logeo.java
+        Logeo formularioLogeo = new Logeo();
+        
+        // Centrar el formulario en la pantalla
+        formularioLogeo.setLocationRelativeTo(null);
+        
+        // Hacer visible el formulario
+        formularioLogeo.setVisible(true);
+        
+        // Opcional: Establecer la operación de cierre para que cierre la aplicación al cerrar la ventana
+        //formularioLogeo.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+        
     }
+    
+   
 }
